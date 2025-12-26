bc3cca055c0076b7e1af2bc8c8632aefb5090ddf
stna-ens <eneskis1324@gmail.comm>
2025-12-26 03:13:55 +0300

    made a pixel-art program

---
+++ script.js
@@ -0,0 +1,63 @@
+let isDrawing = false;
+
+document.body.addEventListener("mousedown", () => {
+  isDrawing = true;
+});
+
+document.body.addEventListener("mouseup", () => {
+  isDrawing = false;
+});
+
+const container = document.createElement("div");
+container.classList.add("container");
+document.body.appendChild(container);
+
+function createDefaultGrid() {
+  for (let i = 0; i < 256; i++) {
+    const cell = document.createElement("div");
+    cell.classList.add("cell");
+    container.appendChild(cell);
+  }
+}
+
+createGrid(16);
+
+function whitenCell(e) {
+  const targetCell = e.target;
+  if (e.type === "mouseover" && !isDrawing) return;
+  let currentLight = Number(targetCell.dataset.light);
+  if (currentLight < 100) {
+    currentLight += 10;
+    targetCell.dataset.light = currentLight;
+    targetCell.style.backgroundColor = `hsl(0, 0%, ${currentLight}%)`;
+  }
+}
+
+function createGrid(gridNumber) {
+  container.innerHTML = "";
+  gridNumber = parseInt(gridNumber);
+  if (gridNumber > 0 && gridNumber < 101) {
+    for (let i = 0; i < gridNumber * gridNumber; i++) {
+      const cell = document.createElement("div");
+      cell.classList.add("cell");
+      const cellSize = 100 / gridNumber;
+      cell.style.width = `${cellSize}%`;
+      cell.style.height = `${cellSize}%`;
+      container.appendChild(cell);
+      cell.dataset.light = "20";
+      cell.style.backgroundColor = "hsl(0, 0%, 20%)";
+      cell.addEventListener("mouseover", whitenCell);
+      cell.addEventListener("mousedown", whitenCell);
+    }
+  }
+}
+
+let btn = document.getElementById("changeGridNumber");
+btn.addEventListener("click", () => {
+  let gridNumber = prompt("Enter a number between 1 and 100");
+  if (gridNumber < 1 || gridNumber > 100) {
+    alert("Please enter a number between 1 and 100");
+  } else {
+    createGrid(gridNumber);
+  }
+});
